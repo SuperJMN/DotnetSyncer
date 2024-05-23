557948748669cf5d6ab68634b1f4b62a5348d335
José Manuel Nieto <superjmn@outlook.com>
2024-05-23 12:36:10 +0200

    Add project files.
    Rework
    
    Setup Nuke
    
    Add GitVersion

---
+++ build/Build.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.GitVersion;
+using Serilog;
+using Zafiro.Nuke;
+using CSharpFunctionalExtensions;
+
+class Build : NukeBuild
+{
+    [GitVersion] readonly GitVersion GitVersion;
+    [Parameter] [Secret] readonly string NuGetApiKey;
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository Repository;
+    
+    public static int Main () => Execute<Build>(x => x.PublishNuget);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    Target Clean => d => d
+        .Executes(() =>
+        {
+            var absolutePaths = RootDirectory.GlobDirectories("**/bin", "**/obj").Where(a => !((string) a).Contains("build")).ToList();
+            Log.Information("Deleting {Dirs}", absolutePaths);
+            absolutePaths.DeleteDirectories();
+        });
+
+    Target PublishNuget => d => d
+        .Requires(() => NuGetApiKey)
+        .DependsOn(Clean)
+        .OnlyWhenStatic(() => Repository.IsOnMainOrMasterBranch())
+        .Executes(() =>
+        {
+            var actions = new Actions(Solution, Repository, RootDirectory, GitVersion, Configuration);
+            actions.PushNuGetPackages(NuGetApiKey)
+                .TapError(error => throw new ApplicationException(error));
+        });
+}
---
+++ src/DotnetSyncer.Console/Core/SeaweedFSPlugin.cs
@@ -0,0 +1,10 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace AvaloniaSyncer.Console.Core;
+
+public class SeaweedFSPlugin : IPlugin
+{
+    public string Name => "seaweedfs";
+    public string FriendlyName  => "SeaweedFS";
+    public Task<Result<ISyncFileSystem>> Create(string args) => SeaweedFS.Create(args).Map(x => (ISyncFileSystem)x);
+}
